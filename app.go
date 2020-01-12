37641b1e8a9cb38581df53c3c25d22926fa3d943
dwdraju <rajudawadinp@gmail.com>
2020-01-12 23:31:21 +0545

    initialize slack rest api call

--- app.go
+++ app.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"github.com/nlopes/slack"
+)
 
-func main(){
-	fmt.Println("Hey! Let's rock K8s with Slack !!!")
+func main() {
+	SLACK_TOKEN := os.Getenv("SLACK_TOKEN")
+	api := slack.New(SLACK_TOKEN)
+  user, err := api.GetUserInfo("U045U8KN6")
+  if err != nil {
+	  fmt.Printf("%s\n", err)
+	  return
+  }
+  fmt.Printf("ID: %s, Fullname: %s, Email: %s\n", user.ID, user.Profile.RealName, user.Profile.Email)
 }
